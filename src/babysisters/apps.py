5f1e9bcd766ef8cc87eaae8bf6776844bbd92a00
Carlos Amaya <carlos.amaya@unillanos.edu.co>
2022-05-04 22:23:03 -0500

    signals

--- src/babysisters/apps.py
+++ src/babysisters/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
+
 class BabysistersConfig(AppConfig):
     name = 'babysisters'
     verbose_name = _('Baby sister')
+
+
--- src/bookings/apps.py
+++ src/bookings/apps.py
@@ -5,3 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class BookingsConfig(AppConfig):
     name = 'bookings'
     verbose_name = _('Bookings')
+
+    def ready(self):
+        import bookings.signals  # pylint: disable=C0415 W0611
